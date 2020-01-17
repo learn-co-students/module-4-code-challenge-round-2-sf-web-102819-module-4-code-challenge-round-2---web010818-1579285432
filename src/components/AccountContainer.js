9f96ca4445d1179a6614b44048f447b56667f56f
Will <widiep@gmail.com>
2020-01-17 11:25:16 -0800

    Complete

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,29 +1,67 @@
 import React, { Component } from 'react'
 import TransactionsList from './TransactionsList'
 import Search from './Search'
-import {transactions} from '../transactionsData'
+// import {transactions} from '../transactionsData'
 
 class AccountContainer extends Component {
-
   constructor() {
     super()
 
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
-
+    this.state = {
+      allTransactions: [],
+      displayTransactions: [],
+      searchTerm: ''
+    }
   }
 
-  handleChange(event) {
+  componentDidMount() {
+    fetch('https://boiling-brook-94902.herokuapp.com/transactions')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          allTransactions: data,
+          displayTransactions: data
+        })
+      })
+  }
+
+  handleChange = event => {
     // your code here
+    console.log(event.target.value)
+    if (event.target.value !== '') {
+      const newDisplayTransaction = this.state.allTransactions.filter(
+        transaction =>
+          transaction.description
+            .toLowerCase()
+            .startsWith(event.target.value.toLowerCase()) ||
+          transaction.category
+            .toLowerCase()
+            .startsWith(event.target.value.toLowerCase())
+      )
+
+      console.log(newDisplayTransaction)
+      this.setState({
+        displayTransactions: newDisplayTransaction,
+        searchTerm: event.target.value
+      })
+    } else {
+      this.setState({
+        displayTransactions: this.state.allTransactions,
+        searchTerm: event.target.value
+      })
+    }
   }
 
   render() {
-
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} />
+        <TransactionsList
+          renderAllTransactions={this.state.displayTransactions}
+        />
       </div>
     )
   }
