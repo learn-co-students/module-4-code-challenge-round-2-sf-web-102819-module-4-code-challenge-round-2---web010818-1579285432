9cf589c955492e46cf78f87e375209390625e044
aryaziai <arya721@gmail.com>
2020-01-17 12:06:47 -0800

    mod 4 retake

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,32 +1,72 @@
-import React, { Component } from 'react'
-import TransactionsList from './TransactionsList'
-import Search from './Search'
-import {transactions} from '../transactionsData'
+import React, { Component } from "react";
+import TransactionsList from "./TransactionsList";
+import Search from "./Search";
+// import {transactions} from '../transactionsData'
 
 class AccountContainer extends Component {
-
   constructor() {
-    super()
+    super();
 
-    // get a default state working with the data imported from TransactionsData
-    // use this to get the functionality working
-    // then replace the default transactions with a call to the API
-
+    this.state = {
+      allTransactions: [],
+      filteredTransactions: []
+    };
   }
 
-  handleChange(event) {
-    // your code here
+  componentDidMount() {
+    fetch("https://boiling-brook-94902.herokuapp.com/transactions")
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          allTransactions: result,
+          filteredTransactions: result
+        });
+      });
   }
+
+  handleChange = event => {
+
+    let filterDescription = this.state.filteredTransactions.filter(transaction =>
+      transaction.description.toLowerCase().startsWith(event.target.value.toLowerCase())
+    );
+
+    let filterCategory = this.state.filteredTransactions.filter(transaction =>
+      transaction.category.toLowerCase().startsWith(event.target.value.toLowerCase())
+    ); // searches Category keys in api.. looking to combined this with filterDescription
+
+ 
+
+    if (event.target.value !== "") {
+      this.setState({
+        filteredTransactions: filterDescription
+      });
+
+
+
+    } else {
+      this.setState({
+        filteredTransactions: this.state.allTransactions
+      });
+    }
+
+    // if (this.state.filteredTransactions === []) {       // not enough to implement category search
+    //   this.setState({
+    //     filteredTransactions:  filterCategory
+    //   })
+    // }
+
+  };
 
   render() {
-
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} />
+        <TransactionsList
+          filteredTransactions={this.state.filteredTransactions}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default AccountContainer
+export default AccountContainer;
